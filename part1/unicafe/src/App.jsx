68bc448bfa5473052787dc7eff337c23035f50ba
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-03 10:37:07 -0500

    1.6(2)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,22 +8,15 @@ function App() {
   const [bad, setBad] = useState(0)
   
   const title = ['give feedback', 'statics']
+  const statics = ['good','neutral', 'bad']
+
+  const handleClickGood = () => setGood(good + 1)
+
+  const handleClickNeutral = () => setNeutral(neutral + 1)
+
+  const handleClickBad = () => setBad(bad + 1)
 
   const Header = ({title}) => <h1>{title}</h1>
-  console.log('firts title is...', title[0])
-  console.log('second title is...', title[1])
-
-  const handleClickGood = () => {
-    console.log('button good is clicked')
-  }
-
-  const handleClickNeutral = () => {
-    console.log('button neutral is clicked')
-  }
-
-  const handleClickBad = () => {
-    console.log('button bad is clicked')
-  }
 
   const Button = ({handleClick, text}) => (
     <button onClick={handleClick}>
@@ -31,13 +24,32 @@ function App() {
     </button>
   )
 
+  const Statics = ({statics, good, neutral, bad}) => {
+    return(
+      <>
+      <p>{statics[0]} {good}</p>
+      <p>{statics[1]} {neutral}</p>
+      <p>{statics[2]} {bad}</p>
+      </>
+    )
+  }
+
+  console.log(title[0])
+  console.log(title[1])
+  statics.forEach (value => {console.log(value)})
+  
   return (
     <>
-      <Header title={title[0]}/>
+      <Header title={title[0]} />
       <Button handleClick={handleClickGood} text= 'good' />
       <Button handleClick={handleClickNeutral} text= 'neutral' />
       <Button handleClick={handleClickBad} text= 'bad' />
-      <Header title={title[1]}/>
+      <Header title={title[1]} />
+      <Statics statics={statics} 
+                good={good} 
+                neutral={neutral}
+                bad={bad}
+      />
 
     </>
   )
