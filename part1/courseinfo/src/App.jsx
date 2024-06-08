80d0a354a9b5970e76ace41615936b9eb7906240
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-08 12:22:27 -0500

    se definen los componentes correctamente

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,6 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+const Header = (props) => {
+  console.log('componente header exitoso')
+  console.log(props.course.name)  
+  return (
+    <>
+      <h1>{props.course.name}</h1>
+    </>
+  )
+}
+
+
+const Content = (props) => {
+  console.log('componente content exitoso') 
+  return (
+   <Part parts={props.parts} />
+  )
+}
+
+const Part = (props) => {
+  console.log('componente part exitoso')
+  console.log(props.parts)
+  return (
+    <>
+    <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>  
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log('componente total exitoso')
+  console.log(props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises)
+  return (
+    <>
+      Number of excercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </>
+  )
+}
 
 const App = () => {
 
@@ -21,51 +66,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
-
-  const Header = (props) => {
-    console.log('componente header exitoso')
-    console.log(props.course.name)  
-    return (
-      <>
-        <h1>{props.course.name}</h1>
-      </>
-    )
-  }
-
-  const Content = (props) => {
-    console.log('componente content exitoso') 
-    return (
-     <Part parts={props.parts} />
-    )
-  }
-
-  const Part = (props) => {
-    console.log('componente part exitoso')
-    console.log(props.parts)
-    return (
-      <>
-      <p>
-          {props.parts[0].name} {props.parts[0].exercises}
-        </p>  
-        <p>
-          {props.parts[1].name} {props.parts[1].exercises}
-        </p>
-        <p>
-          {props.parts[2].name} {props.parts[2].exercises}
-        </p>
-      </>
-    )
-  }
-
-  const Total = (props) => {
-    console.log('componente total exitoso')
-    console.log(props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises)
-    return (
-      <>
-        Number of excercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-      </>
-    )
   }
 
   return(
