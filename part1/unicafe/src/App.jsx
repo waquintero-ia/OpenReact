59fcc936e20435a28d31bf62fac481403f659dba
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-08 12:19:56 -0500

    se definen los componentes correctamente

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,64 @@
 import { useState } from 'react'
 
+const Header = ({title}) => <h1>{title}</h1>
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  let all = good + neutral + bad
+  let average = (good - bad) / all
+  let positive = (good / all) * 100
+  if (good === 0 && neutral === 0 && bad === 0){
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return(
+    <>
+    <StatisticLine text='good' value={good}/>
+    <StatisticLine text='neutral' value={neutral}/>
+    <StatisticLine text='bad' value={bad}/>
+    <StatisticLine text='all' value={all}/>
+    <StatisticLine text='average' value={average.toFixed(1)}/>
+    <StatisticLine text='positive' value={positive.toFixed(1)}/>
+    </>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+if (text === 'positive'){
+  return (
+  <>
+    <table>
+      <tbody>
+        <tr>
+        <td style={{width:"60px"}}>{text}</td>
+        <td style={{textAlign:"left", width:"60px"}}> {value} %</td>
+        </tr>
+      </tbody>
+    </table>
+  </>  
+  )
+}  
+  return(
+    <>
+    <table>
+      <tbody>
+        <tr>
+        <td style={{width:"60px"}}>{text}</td>
+        <td style={{textAlign:"left", width:"60px"}}> {value}</td>
+        </tr>
+      </tbody>
+    </table>
+    </>
+  )
+}
 
 function App() {
   const [good, setGood] = useState(0)
@@ -14,66 +72,6 @@ function App() {
   const handleClickNeutral = () => setNeutral(neutral + 1)
 
   const handleClickBad = () => setBad(bad + 1)
-
-  const Header = ({title}) => <h1>{title}</h1>
-
-  const Button = ({handleClick, text}) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-
-  const Statistics = ({good, neutral, bad}) => {
-    let all = good + neutral + bad
-    let average = (good - bad) / all
-    let positive = (good / all) * 100
-    if (good === 0 && neutral === 0 && bad === 0){
-      return(
-        <div>
-          No feedback given
-        </div>
-      )
-    }
-    return(
-      <>
-      <StatisticLine text='good' value={good}/>
-      <StatisticLine text='neutral' value={neutral}/>
-      <StatisticLine text='bad' value={bad}/>
-      <StatisticLine text='all' value={all}/>
-      <StatisticLine text='average' value={average.toFixed(1)}/>
-      <StatisticLine text='positive' value={positive.toFixed(1)}/>
-      </>
-    )
-  }
-
-  const StatisticLine = ({text, value}) => {
-  if (text === 'positive'){
-    return (
-    <>
-      <table>
-        <tbody>
-          <tr>
-          <td style={{width:"60px"}}>{text}</td>
-          <td style={{textAlign:"left", width:"60px"}}> {value} %</td>
-          </tr>
-        </tbody>
-      </table>
-    </>  
-    )
-  }  
-    return(
-      <>
-      <table>
-        <tbody>
-          <tr>
-          <td style={{width:"60px"}}>{text}</td>
-          <td style={{textAlign:"left", width:"60px"}}> {value}</td>
-          </tr>
-        </tbody>
-      </table>
-      </>
-    )
-  }
 
   console.log(title[0])
   console.log(title[1])
