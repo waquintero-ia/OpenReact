d6c92415d194751e9044c26c96148a47e12a86a5
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-08 14:40:45 -0500

    2.1 y 2.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,66 +13,72 @@ const Header = (props) => {
 }
 
 
-const Content = (props) => {
+const Content = ({parts}) => {
   console.log('componente content exitoso') 
   return (
-   <Part parts={props.parts} />
+   <Part parts={parts} />
   )
 }
 
-const Part = (props) => {
+const Part = ({parts}) => {
   console.log('componente part exitoso')
-  console.log(props.parts)
+  console.log(parts)
   return (
     <>
-    <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>  
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
+    {parts.map(part =>
+      <p key={part.id}> {part.name} {part.exercises}</p>
+    )}
     </>
   )
 }
 
-const Total = (props) => {
+const Total = ({total}) => {
   console.log('componente total exitoso')
-  console.log(props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises)
-  return (
+  let sum = 0
+  total.forEach(x => {
+    sum += x.exercises
+  });
+  console.log(sum);
+ return ( 
     <>
-      Number of excercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      Number of excercises {sum}
     </>
   )
 }
 
 const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const course = {
+  id:1,
+  name:'Half Stack application development',
+  parts: [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10,
+    id: 1
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7,
+    id: 2
+  },
+  {
+    name: 'State of a component',
+    exercises: 14,
+    id: 3
+  },
+  {
+    name: 'Redux',
+    exercises: 11,
+    id:4
   }
 
+  ]
+}
   return(
     <>
       <Header course={course} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total total={course.parts} />
     </>
   )
 }
