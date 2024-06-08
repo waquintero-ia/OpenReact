a83497cea53a18d1a00a0e1e3573861f5aac8de7
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-08 12:14:07 -0500

    version final

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,26 +13,29 @@ const Button = ({handleClic, text}) =>{
   )
 }
 
-const Votes = ({selected, votes}) =>{
-  console.log('componente votes exitoso')
-  console.log('points is...', votes[selected])
-  console.log('selected item...', selected)
-
+const Point = ({point}) => {
   return(
-    <div>has {votes[selected]} votes</div>
+    <div>has {point} votes</div>
+  )
+}
+
+const Votes = ({point}) =>{
+  console.log('componente votes exitoso')
+  return(
+    <Point point={point}/>
   )
 }
 
 const Title = ({title}) => <h1>{title}</h1>
 
-const TheMostAnecdote = ({max}) =>{
+const TheMostAnecdote = ({maxItem, anecdotes, maxPoint}) =>{
   
   console.log('component the most active')
-  console.log('component the most active', max)
   //console.log('the higer point is...', max)
-
   return(
     <>
+      <div>{anecdotes[maxItem]}</div>
+      <Point point={maxPoint}/>
     </>
   )
 }
@@ -40,6 +43,9 @@ const TheMostAnecdote = ({max}) =>{
 const App = () =>{
 
 const [selected, setSelect] = useState(0)
+const [currentPoint, setCurrentPoint] = useState(0)
+const [maxPoint, setMaxPoint] = useState(0)
+const [maxItem, setMaxItem] = useState(0)
 
 const title = ['Anecdote of the day', 'Anecdote with most votes']
 
@@ -60,6 +66,7 @@ const anecdotes = [
     random === selected ? random = (Math.random()*(0.7)).toFixed(1)*10 : random
     console.log('random is...', random)
     setSelect(random)
+    setCurrentPoint(copy[random])
   }
 
   const handleVote = () =>{
@@ -67,17 +74,35 @@ const anecdotes = [
     console.log('select for vote is...', selected)
     copy[selected] += 1
     console.log(copy)
+    setCurrentPoint(copy[selected])
+    let maxValue = 0
+    let maxItem = 0
+    Object.keys(copy).forEach((e) =>{
+      console.log(e, ':', copy[e])
+      if (copy[e] > maxValue){
+        maxValue = copy[e]
+        maxItem = e
+      }
+      console.log('max value is...', maxValue)
+      console.log('current point...', copy[selected])
+      console.log ('max item is...', maxItem)
+      console.log ('current item is...', selected)
+    })
+
+    setMaxPoint(maxValue)
+    setMaxItem(maxItem)
   }
 
   return(
     <>
+      
       <Title title={title[0]}/>
       <div>{anecdotes[selected]}</div>
-      <Votes selected={selected} votes={copy}/>
-      <Button handleClic= {handleVote} text='vote' />
+      <Votes point={currentPoint}/>
+      <Button handleClic= {handleVote} text='vote'  />
       <Button handleClic={handleNext} text='next anecdote'/>
       <Title title={title[1]}/>
-      <TheMostAnecdote max={copy}/>
+      <TheMostAnecdote maxItem={maxItem} anecdotes={anecdotes} maxPoint = {maxPoint}/>
     </>
   )
 }
