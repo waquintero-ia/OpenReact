6b620be757b0f5575fbb5b46a1c6e52dd40ef17c
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-10 12:27:07 -0500

    2.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,40 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-const Header = (props) => {
+const Header = ({course, index}) => {
   console.log('componente header exitoso')
-  console.log(props.course.name)  
+  console.log(course[index].id)
+  let titleCourse = course[index].name
+  let idCourse = course[index].id 
   return (
     <>
-      <h1>{props.course.name}</h1>
+      <h1 key={idCourse}>{titleCourse}</h1>
     </>
   )
 }
 
 
-const Content = ({parts}) => {
+const Content = ({parts, index}) => {
   console.log('componente content exitoso') 
   return (
-   <Part parts={parts} />
+   <Part parts={parts} index={index}/>
   )
 }
 
-const Part = ({parts}) => {
+const Part = ({parts, index}) => {
   console.log('componente part exitoso')
-  console.log(parts)
+  console.log(parts[index].parts)
+  let partsName = parts[index].parts
+  console.log(partsName);
+  /*console.log('que sucede con parts...',parts.map(part[0] => part.parts));*/
   return (
     <>
-    {parts.map(part =>
-      <p key={part.id}> {part.name} {part.exercises}</p>
-    )}
+      {partsName.map(partsName => 
+        <p key={partsName.id}>{partsName.name} {partsName.exercises}</p>
+      )}
     </>
   )
 }
 
-const Total = ({total}) => {
+const Total = ({total, index}) => {
   console.log('componente total exitoso')
-  const inicial = 0
-  let sum = total.map(part => part.exercises)
+  let partsExercises = total[index].parts
+  console.log(partsExercises);
+  let sum = partsExercises.map(partsExercises => partsExercises.exercises)
   console.log('la suma es...',sum);
   const totales = sum.reduce((s,p) => {
     console.log('what is happening...', s, p);
@@ -43,44 +49,65 @@ const Total = ({total}) => {
   })
  return ( 
     <>
-      Number of excercises {totales}
+      total of {totales} exercises
     </>
   )
 }
 
 const App = () => {
-const course = {
-  id:1,
-  name:'Half Stack application development',
-  parts: [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10,
-    id: 1
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7,
-    id: 2
-  },
-  {
-    name: 'State of a component',
-    exercises: 14,
-    id: 3
-  },
-  {
-    name: 'Redux',
-    exercises: 11,
-    id:4
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
   ]
-}
+
   return(
     <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
+    <Header course={courses} index={0}/>
+     <Content parts={courses} index={0}/>
+      <Total total={courses} index={0}/>
+      <Header course={courses} index={1}/>
+     <Content parts={courses} index={1}/>
+      <Total total={courses} index={1}/>
     </>
   )
 }
