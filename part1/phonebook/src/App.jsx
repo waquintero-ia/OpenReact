024545e3d4731d8e680ca1c51724c94a769ad94b
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-10 16:52:23 -0500

    2.6

--- part1/phonebook/src/App.jsx
+++ part1/phonebook/src/App.jsx
@@ -1,11 +1,53 @@
 import { useState } from 'react'
+import Persons from './components/Persons'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      id: 1,
+      name: 'Arto Hellas'
+    }
+  ])
 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('add clicked...', event.target);
+
+    const newObject = {
+      id: persons.length + 1,
+      name: newName
+    }
+    setPersons(persons.concat(newObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  console.log(persons)
   return (
-    <>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{listStyleType:"none"}}>
+        {persons.map(persons => 
+          <Persons key={persons.id} persons={persons} />
+        )}
+      </ul>
+      
+    </div>
   )
 }
 
---
+++ part1/phonebook/src/components/Persons.jsx
@@ -0,0 +1,9 @@
+const Persons = ({persons}) => {
+    return(
+        <li>
+            {persons.name}
+        </li>
+    )
+}
+
+export default Persons
